490143fba70d17d392b733bbe4afd2918dfe87be
babycheese <84229686+babycheese@users.noreply.github.com>
2022-07-16 21:53:15 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import ColorfullMessage from "./compornents/ColorfullMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+  useState();
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
+      <ColorfullMessage color="pink">元気です</ColorfullMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
