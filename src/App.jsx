0d354fb821bd19955459f1888097466066ded4b1
babycheese <84229686+babycheese@users.noreply.github.com>
2022-07-18 06:09:27 +0900

    修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfullMessage from "./compornents/ColorfullMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  useState();
+  console.log("コンポーネントの最初");
+  // state
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCount = () => {
+    setNum(num + 1);
+  };
+  const onClickFace = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  useEffect(() => {
+    console.log("useEffect!");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です</ColorfullMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCount}>カウントアップ</button>
+      <br />
+      <button onClick={onClickFace}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(*´Д｀)</p>}
     </>
   );
 };
